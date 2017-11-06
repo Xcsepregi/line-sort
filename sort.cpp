a4ff0587daf7cb2642cdac3c59a7e45c058bcee3
Xcsepregi <csepregi.nikolas@gmail.com>
2017-11-06 10:09:26 +0100

    Doplnujuci commit

--- sort.cpp
+++ sort.cpp
@@ -16,26 +16,11 @@ private:
 	bool is_case_sensitive;
 };
 
-void sort_ascend_descend(int ord, std::vector<std::string> &lines)
-{
-	(ord == 0) ? std::sort(lines.begin(), lines.end()) : std::reverse(lines.begin(), lines.end());
-}
-
-void sort_case_ignore(std::vector<std::string> &lines)
-{
-	std::sort(lines.begin(), lines.end(), StringCompare(false));
-}
-
 template <class T>
 void UniqueSort(std::vector<T> & lines)
 {
 	set<T> values;
 	lines.erase(std::remove_if(lines.begin(), lines.end(), [&](const T & value) { return !values.insert(value).second; }), lines.end());
-}
-
-void main_sort(int stat_ord, int stat_fil, int stat_comp,std::vector<std::string> &lines)
-{
-	
 }
 
 namespace
@@ -66,8 +51,12 @@ bool sort::process(Order order, Filter filter, Case compare, std::istream & inpu
 	(filter == Filter::all) ? stat_fil = 0 : stat_fil = 1;
 	(compare == Case::ignore) ? stat_comp = 0 : stat_comp = 1;
 
-	main_sort(stat_ord, stat_fil, stat_comp,riadky);
+	(stat_ord == 0) ? std::sort(lines.begin(), lines.end()) : std::reverse(lines.begin(), lines.end());
+	if (stat_fil == 1) UniqueSort(lines);
+	if(stat_comp == 1) std::sort(lines.begin(), lines.end(), StringCompare(false));
 
+	for (std::string ret : lines) output << ret << std::endl;
+	
 	return true;
 }
 
